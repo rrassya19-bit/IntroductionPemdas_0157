06fa75d6a56e32e92497e66de8004486fd84a170
rrassya19-bit <rrassys19@gmail.com>
2026-02-23 15:24:51 +0700

    first commit

---
+++ Hitung_Luas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {  //mulai
+    int p, l, luas;
+
+    //langkah 2
+    cout << "Masukkan panjang: ";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukkan lebar: ";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "Haasilnya= " << luas;   
+}  //selesai
---
+++ P2.cpp
@@ -0,0 +1,15 @@
+//contoh libbary
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main() {  //program utama
+    int age;  //variable local
+    age = 20;
+
+    cout << "Hello, World" << endl;
+    cout << 99 << endl;
+    cout << age;
+}
