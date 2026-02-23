70b9f614e39448fcea06c2c0f075fa208a663ff9
rrassya19-bit <rrassys19@gmail.com>
2026-02-23 15:49:23 +0700

    menambah int num = 10; and cout << int(num) << endl;  //casting

--- P2.cpp
+++ P2.cpp
@@ -8,8 +8,10 @@ string nama;
 int main() {  //program utama
     int age;  //variable local
     age = 20;
+    int num = 10;
 
     cout << "Hello, World" << endl;
     cout << 99 << endl;
     cout << age;
+    cout << int(num) << endl;  //casting
 }
